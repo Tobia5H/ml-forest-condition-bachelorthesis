cf00ab4562fccc99d66dfd9e1adbf520d7be732e
Tobias Hohendanner <e11941112@student.tuwien.ac.at>
2024-08-20 04:56:28 +0200

    Add multiple vegetation indices.

--- sentinal2downloader.py
+++ sentinal2downloader.py
@@ -39,7 +39,7 @@ class Sentinel2Downloader:
         # Filter to ensure images are taken during daylight
         solar_elevation_filter = ee.Filter.gt('MEAN_SOLAR_ZENITH_ANGLE', 0)  # Ensure daylight by positive solar elevation
         
-        sentinel2 = ee.ImageCollection('COPERNICUS/S2_HARMONIZED') \
+        sentinel2 = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED') \
                     .filterDate(start_date, end_date) \
                     .filterBounds(aoi) \
                     .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 10)) \
@@ -103,6 +103,115 @@ class Sentinel2Downloader:
         
         self.logger.info(f"NDVI download URL generated: {download_url}")
         return download_url
+    
+    def get_evi_download_url(self, aoi, start_date, end_date, scale=10, crs='EPSG:25833'):
+        """Generate a download link for the EVI image.
+        
+        Args:
+            aoi (ee.Geometry): Area of interest as an ee.Geometry object.
+            start_date (str): Start date for filtering the image collection (YYYY-MM-DD).
+            end_date (str): End date for filtering the image collection (YYYY-MM-DD).
+            scale (int, optional): Spatial resolution of the output image in meters. Default is 10.
+            crs (str, optional): Coordinate Reference System for the output image. Default is 'EPSG:25833'.
+        
+        Returns:
+            str: URL link to download the EVI image.
+        """
+        self.logger.info("Generating EVI download URL.")
+        median_image = self.get_median_image(aoi, start_date, end_date)
+        
+        evi_image = median_image.expression(
+            '2.5 * ((NIR - RED) / (NIR + 6 * RED - 7.5 * BLUE + 1))',
+            {
+                'NIR': median_image.select('B8'),
+                'RED': median_image.select('B4'),
+                'BLUE': median_image.select('B2')
+            }
+        ).rename('EVI')
+        
+        download_url = evi_image.getDownloadURL({
+            'scale': scale,
+            'region': aoi.getInfo()['coordinates'],
+            'crs': crs,
+            'format': 'GeoTIFF'
+        })
+        
+        self.logger.info(f"EVI download URL generated: {download_url}")
+        return download_url
+
+    def get_gndvi_download_url(self, aoi, start_date, end_date, scale=10, crs='EPSG:25833'):
+        """Generate a download link for the GNDVI image.
+        
+        Args:
+            aoi (ee.Geometry): Area of interest as an ee.Geometry object.
+            start_date (str): Start date for filtering the image collection (YYYY-MM-DD).
+            end_date (str): End date for filtering the image collection (YYYY-MM-DD).
+            scale (int, optional): Spatial resolution of the output image in meters. Default is 10.
+            crs (str, optional): Coordinate Reference System for the output image. Default is 'EPSG:25833'.
+        
+        Returns:
+            str: URL link to download the GNDVI image.
+        """
+        self.logger.info("Generating GNDVI download URL.")
+        median_image = self.get_median_image(aoi, start_date, end_date)
+        gndvi_image = median_image.normalizedDifference(['B8', 'B3']).rename('GNDVI')
+
+        download_url = gndvi_image.getDownloadURL({
+            'scale': scale,
+            'region': aoi.getInfo()['coordinates'],
+            'crs': crs,
+            'format': 'GeoTIFF'
+        })
+        
+        self.logger.info(f"GNDVI download URL generated: {download_url}")
+        return download_url
+
+    def get_chlorophyll_index_download_url(self, aoi, start_date, end_date, scale=10, crs='EPSG:25833', index_type='green'):
+        """Generate a download link for the Chlorophyll Index image.
+        
+        Args:
+            aoi (ee.Geometry): Area of interest as an ee.Geometry object.
+            start_date (str): Start date for filtering the image collection (YYYY-MM-DD).
+            end_date (str): End date for filtering the image collection (YYYY-MM-DD).
+            scale (int, optional): Spatial resolution of the output image in meters. Default is 10.
+            crs (str, optional): Coordinate Reference System for the output image. Default is 'EPSG:25833'.
+            index_type (str, optional): Type of Chlorophyll Index ('green' or 'red-edge'). Default is 'green'.
+        
+        Returns:
+            str: URL link to download the Chlorophyll Index image.
+        """
+        self.logger.info(f"Generating Chlorophyll Index ({index_type}) download URL.")
+        median_image = self.get_median_image(aoi, start_date, end_date)
+        
+        if index_type == 'green':
+            chlorophyll_index_image = median_image.expression(
+                '(NIR / GREEN) - 1',
+                {
+                    'NIR': median_image.select('B8'),
+                    'GREEN': median_image.select('B3')
+                }
+            ).rename('CIgreen')
+        elif index_type == 'red-edge':
+            chlorophyll_index_image = median_image.expression(
+                '(NIR / REDEDGE) - 1',
+                {
+                    'NIR': median_image.select('B8'),
+                    'REDEDGE': median_image.select('B5')
+                }
+            ).rename('CIred-edge')
+        else:
+            raise ValueError("Invalid index_type. Use 'green' or 'red-edge'.")
+        
+        download_url = chlorophyll_index_image.getDownloadURL({
+            'scale': scale,
+            'region': aoi.getInfo()['coordinates'],
+            'crs': crs,
+            'format': 'GeoTIFF'
+        })
+        
+        self.logger.info(f"Chlorophyll Index ({index_type}) download URL generated: {download_url}")
+        return download_url
+
 
     def download_nvdi_image(self, longitude_min, latitude_min, longitude_max, latitude_max, start_date, end_date, crs='EPSG:25833'):
         """Download links for NDVI image using the bounding box coordinates.
@@ -163,6 +272,25 @@ class Sentinel2Downloader:
         nvdi_path = self.download_nvdi_image(longitude_min, latitude_min, longitude_max, latitude_max, start_date, end_date, crs)
         rgb_path = self.download_rgb_image(longitude_min, latitude_min, longitude_max, latitude_max, start_date, end_date, crs)
         return [rgb_path, nvdi_path]
+    
+    def download_evi_image(self, longitude_min, latitude_min, longitude_max, latitude_max, start_date, end_date, crs='EPSG:25833'):
+        self.logger.info("Downloading EVI image.")
+        aoi = ee.Geometry.Rectangle([longitude_min, latitude_min, longitude_max, latitude_max])
+        evi_url = self.get_evi_download_url(aoi=aoi, start_date=start_date, end_date=end_date, crs=crs)
+        return self._get_image_with_request(evi_url, "evi")
+
+    def download_gndvi_image(self, longitude_min, latitude_min, longitude_max, latitude_max, start_date, end_date, crs='EPSG:25833'):
+        self.logger.info("Downloading GNDVI image.")
+        aoi = ee.Geometry.Rectangle([longitude_min, latitude_min, longitude_max, latitude_max])
+        gndvi_url = self.get_gndvi_download_url(aoi=aoi, start_date=start_date, end_date=end_date, crs=crs)
+        return self._get_image_with_request(gndvi_url, "gndvi")
+
+    def download_chlorophyll_index_image(self, longitude_min, latitude_min, longitude_max, latitude_max, start_date, end_date, crs='EPSG:25833', index_type='green'):
+        self.logger.info(f"Downloading Chlorophyll Index ({index_type}) image.")
+        aoi = ee.Geometry.Rectangle([longitude_min, latitude_min, longitude_max, latitude_max])
+        ci_url = self.get_chlorophyll_index_download_url(aoi=aoi, start_date=start_date, end_date=end_date, crs=crs, index_type=index_type)
+        return self._get_image_with_request(ci_url, f"chlorophyll_{index_type}")
+
             
     def _get_image_with_request(self, url, output_filename):
         """Helper method to download an image from a URL.
